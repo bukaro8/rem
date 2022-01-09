b2eeb63367c13a4de6e34a565008f6ed7a74ebff
Victor Ramirez <vicdeveloper83@gmail.com>
2022-01-09 22:42:32 +0000

    finished

--- js/MenuItems.js
+++ js/MenuItems.js
@@ -2,7 +2,7 @@
 export const menu=[
   {url:'https://developer.mozilla.org/en-US/docs/Learn/CSS/Building_blocks/Values_and_units',
   name:   'Whats REM'},
-  {url:   'https://google.com',
+  {url:   'https://www.croydon.gov.uk/design-and-content-guidelines/resources/pixel-rem-conversion-table',
   name:   'Time-table'},
   {url:   '#',
   name:   "FAQ's"},
